f1854db34ec25e4871659095f9fda1239423c2bd
Unknown <Arnaud.Savary@etu.unige.ch>
2021-08-23 20:17:15 +0200

    Removal of useless files

--- CryptItClient/test_GUI.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter.messagebox import *
-import tkinter.scrolledtext as st
-
-window = Tk()
-window.title('CryptItClient')
-window.geometry("550x450+500+200")
-groupChatFrame('Arnaud','MyGroup')
-window.mainloop()
